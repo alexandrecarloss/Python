98c7abf0bad6e0e3c620eed9cfaf67dfe41b5145
alexandrecarloss <107065086+alexandrecarloss@users.noreply.github.com>
2022-08-04 14:47:17 -0400

    aula 9

---
+++ teste/aula09-1.py
@@ -0,0 +1,16 @@
+frase = 'Curso em Vídeo Python'
+print(frase.count('O'))
+print(frase.upper().count('O'))
+print(len(frase))
+print(frase.replace('Python', 'Android'))
+print(frase)
+# frase = frase.replace('Python', 'Android')
+# print(frase)
+print('Curso' in frase)
+print(frase.find('Curso'))
+print(frase.lower().find('vídeo'))
+#print(frase.split()[3])
+dividido = frase.split()
+print(dividido[2][3])
+
+
