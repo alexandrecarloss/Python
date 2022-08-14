56eba51ea4439bd720639e3ae8405c22cbbc299a
alexandrecarloss <107065086+alexandrecarloss@users.noreply.github.com>
2022-08-14 12:53:21 -0400

    Aula 14

---
+++ teste/aula14-1.py
@@ -0,0 +1,8 @@
+'''for c in range(1, 10):
+    print(c)
+print('FIM!')'''
+r = 'S'
+while r == 'S':
+    c = int(input('Digite um valor: '))
+    r = input('Quer contiuar? [S/N]: ').upper()
+print('FIM')
