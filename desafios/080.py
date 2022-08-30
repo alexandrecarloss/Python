7530aa5158120a52b587cce745c1873d537ff36b
alexandrecarloss <107065086+alexandrecarloss@users.noreply.github.com>
2022-08-30 15:43:33 -0400

    Desafio 80

--- desafios/080.py
+++ desafios/080.py
@@ -1,18 +1,16 @@
-num = []
+lista = []
 for c in range(0, 5):
-    valor = int(input('Digite um valor: '))
-    if c == 0 or valor >= (num[len(num) - 1]):
-        num.append(valor)
+    n = int(input('Digite um valor: '))
+    if c == 0 or n > lista[-1]:
+        lista.append(n)
         print('Adicionado ao final da lista...')
-    elif valor < num[0]:
-        num.insert(0, valor)
-        print('Adicionado na posição 0 da lista')
     else:
-        for i, v in enumerate(num):
-            if valor < v:
-                print(f'Índice {i}')
-                print(f'Valor {v}')
-            if valor > v:
-                print(f'Índice {i} maior')
-                print(f'Valor {v} maior')
-    
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                print(f'Adicionado na posição {pos} da lista...')
+                break
+            pos += 1
+print(f'-='*30)
+print(f'Os valores digitados em ordem foram: {lista}')
